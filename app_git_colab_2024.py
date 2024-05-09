7a36e2a9ff6dfd75352860bfe8b02d87ec2603af
fvergara24 <99352255+fvergara24@users.noreply.github.com>
2024-05-08 21:01:57 -0600

    Add files via upload

---
+++ app_git_colab_2024.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title='Herramientas digitales en español', page_icon='favicon.jpg')
+# st.write('↖ Categorias')
+st.image('Logo.jpg')
+st.title('Herramientas digitales en español')
