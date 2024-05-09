d3a8bd8a54540464e9ca3b7943992f68ef0aa2c6
fvergara24 <fer335@gmail.com>
2024-05-09 23:21:09 +0000

    pagina

--- app_git_colab_2024.py
+++ app_git_colab_2024.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title='Herramientas 2024 digitales en español', page_ic
 st.image('Logo.jpg')
 st.title('Herramientas digitales 2024 en español')
 
-social-media=pd.read_csv('social-media.csv')
-social-media=social-media.drop(columns='Unnamed: 0',axis=1)
+socialmedia=pd.read_csv('social-media.csv')
+socialmedia=socialmedia.drop(columns='Unnamed: 0',axis=1)
 
-st.dataframe(social-media)
+st.dataframe(socialmedia)
